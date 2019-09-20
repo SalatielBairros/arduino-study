8293c8549bb734adb2f56d47c34283a321b79292
SALATIEL BAIRROS <salatiel.costabairros@gmail.com>
2019-09-19 21:34:11 -0300

    Testes realizados na aula de Microcontroladores

---
+++ debug_test.ino
@@ -0,0 +1,14 @@
+#include <DHT.h>
+#include <DHT_U.h>
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  Serial.begin(9600);
+  Serial.print("Meu nome é Salatiel.");
+  delay(1000);
+}
